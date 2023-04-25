8a7170aecd808460242db5ad1ce262521c3ad311
diegodorow <diego.dorow@hotmail.com>
2023-04-24 22:55:39 -0300

    criando diretorio da pasta para conexao jpa

---
+++ src/main/java/dsw/projeto/projetodsw/ProjetoDswApplication.java
@@ -0,0 +1,13 @@
+package dsw.projeto.projetodsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDswApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDswApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsw/projeto/projetodsw/ProjetoDswApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw.projeto.projetodsw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
